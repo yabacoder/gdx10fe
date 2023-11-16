d83a8e4ff10b1b895bd56760acbfd0dc4f3b1685
Sheriff Shittu <possicon@Sheriffs-MacBook-Air.local>
2023-11-16 11:48:09 +0100

    company settings edit

--- src/views/company/settings/LeftNav.js
+++ src/views/company/settings/LeftNav.js
@@ -8,22 +8,22 @@ function LeftNav(props) {
     return (  
         <div>
             <div >
-                <Link to="../settings">
-                    <p className={(currentLocation === 'settings') ? 'p-5 font-getdevB  bg-red-100 border-r-4 border-red-600 rounded-r-md' : 'p-5 '}>
+                <Link to="/company/settings">
+                    <p className={(currentLocation === '/company/settings') ? 'p-5 font-getdevB  bg-red-100 border-r-4 border-red-600 rounded-r-md' : 'p-5 '}>
                         Profile
                     </p>
                 </Link>
             </div>
             <div >
-                <Link to="../settings/page">
-                    <p className={(currentLocation === 'settings/page') ? 'p-5 font-getdevB  bg-red-100 border-r-4 border-red-600 rounded-r-md' : 'p-5 '}>
+                <Link to="/company/settings/page">
+                    <p className={(currentLocation === '/company/settings/page') ? 'p-5 font-getdevB  bg-red-100 border-r-4 border-red-600 rounded-r-md' : 'p-5 '}>
                         Page Settings
                     </p>
                 </Link>
             </div>
             <div >
-                <Link to="../settings/password">
-                    <p className={(currentLocation === 'settings/password') ? 'p-5 font-getdevB  bg-red-100 border-r-4 border-red-600 rounded-r-md' : 'p-5 '}>
+                <Link to="/company/settings/password">
+                    <p className={(currentLocation === '/company/settings/password') ? 'p-5 font-getdevB  bg-red-100 border-r-4 border-red-600 rounded-r-md' : 'p-5 '}>
                         Change Password
                     </p>
 
